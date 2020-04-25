9bc84498cdb1bd11028517edea4bee3d93f5baf3
Khanh DQ <khanh.dq@rubycell.com>
2020-04-25 16:03:03 +0700

    up len cho Huy

--- lite/examples/speech_commands/android/app/src/main/java/org/tensorflow/lite/examples/speech/SpeechActivity.java
+++ lite/examples/speech_commands/android/app/src/main/java/org/tensorflow/lite/examples/speech/SpeechActivity.java
@@ -63,37 +63,43 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.concurrent.locks.ReentrantLock;
+
+/*Once you’ve done this you can import a TensorFlow Lite interpreter.
+An Interpreter loads a model and allows you to run it, by providing it with a set of inputs.
+
+TensorFlow Lite will then execute the model and write the outputs, it’s really as simple as that.
+copied here by Khanh
+ */
 import org.tensorflow.lite.Interpreter;
 
 /**
  * An activity that listens for audio and then uses a TensorFlow model to detect particular classes,
  * by default a small set of action words.
  */
-public class SpeechActivity extends Activity
-    implements View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+public class SpeechActivity extends Activity {
 
   // Constants that control the behavior of the recognition code and model
   // settings. See the audio recognition tutorial for a detailed explanation of
   // all these, but you should customize them to match your training settings if
   // you are running your own model.
-  private static final int SAMPLE_RATE = 16000;
-  private static final int SAMPLE_DURATION_MS = 1000;
-  private static final int RECORDING_LENGTH = (int) (SAMPLE_RATE * SAMPLE_DURATION_MS / 1000);
-  private static final long AVERAGE_WINDOW_DURATION_MS = 1000;
-  private static final float DETECTION_THRESHOLD = 0.50f;
-  private static final int SUPPRESSION_MS = 1500;
-  private static final int MINIMUM_COUNT = 3;
-  private static final long MINIMUM_TIME_BETWEEN_SAMPLES_MS = 30;
-  private static final String LABEL_FILENAME = "file:///android_asset/conv_actions_labels.txt";
-  private static final String MODEL_FILENAME = "file:///android_asset/conv_actions_frozen.tflite";
+
+  //Model specific constants by Khanh
+  private static final int MODEL_SAMPLE_RATE = 16000;
+  private static final int MODEL_FRAME_LENGTH = 17920;
+  private static final String MODEL_FILEPATH = "file:///android_asset/onsets_frames_wavinput.tflite";
+  private static final int INPUT_SAMPLE_RATE = 16000; //TODO We need test this value with real hardware
+
 
   // UI elements.
   private static final int REQUEST_RECORD_AUDIO = 13;
   private static final String LOG_TAG = SpeechActivity.class.getSimpleName();
+  private static final long MINIMUM_TIME_BETWEEN_SAMPLES_MS = 10;
+  private static final short AMPLIFY_PARAM = 1; //TODO should be 1.5 or 2
 
   // Working variables.
-  short[] recordingBuffer = new short[RECORDING_LENGTH];
+  short[] recordingBuffer = new short[MODEL_FRAME_LENGTH];
   int recordingOffset = 0;
   boolean shouldContinue = true;
   private Thread recordingThread;
@@ -103,33 +109,18 @@ public class SpeechActivity extends Activity
 
   private List<String> labels = new ArrayList<String>();
   private List<String> displayedLabels = new ArrayList<>();
-  private RecognizeCommands recognizeCommands = null;
-  private LinearLayout bottomSheetLayout;
-  private LinearLayout gestureLayout;
-  private BottomSheetBehavior<LinearLayout> sheetBehavior;
 
-  private Interpreter tfLite;
-  private ImageView bottomSheetArrowImageView;
-
-  private TextView yesTextView,
-      noTextView,
-      upTextView,
-      downTextView,
-      leftTextView,
-      rightTextView,
-      onTextView,
-      offTextView,
-      stopTextView,
-      goTextView;
-  private TextView sampleRateTextView, inferenceTimeTextView;
-  private ImageView plusImageView, minusImageView;
-  private SwitchCompat apiSwitchCompat;
-  private TextView threadsTextView;
   private long lastProcessingTimeMs;
   private Handler handler = new Handler();
   private TextView selectedTextView = null;
   private HandlerThread backgroundThread;
   private Handler backgroundHandler;
+
+  //Init a new interpreter by Khanh
+  private Interpreter tfLite;
+
+  //UI code by Khanh, id = textView
+  private TextView outTextView;
 
   /** Memory-map the model file in Assets. */
   private static MappedByteBuffer loadModelFile(AssetManager assets, String modelFilename)
@@ -148,121 +139,26 @@ public class SpeechActivity extends Activity
     super.onCreate(savedInstanceState);
     setContentView(R.layout.tfe_sc_activity_speech);
 
-    // Load the labels for the model, but only display those that don't start
-    // with an underscore.
-    String actualLabelFilename = LABEL_FILENAME.split("file:///android_asset/", -1)[1];
-    Log.i(LOG_TAG, "Reading labels from: " + actualLabelFilename);
-    BufferedReader br = null;
-    try {
-      br = new BufferedReader(new InputStreamReader(getAssets().open(actualLabelFilename)));
-      String line;
-      while ((line = br.readLine()) != null) {
-        labels.add(line);
-        if (line.charAt(0) != '_') {
-          displayedLabels.add(line.substring(0, 1).toUpperCase() + line.substring(1));
-        }
-      }
-      br.close();
-    } catch (IOException e) {
-      throw new RuntimeException("Problem reading label file!", e);
-    }
 
-    // Set up an object to smooth recognition results to increase accuracy.
-    recognizeCommands =
-        new RecognizeCommands(
-            labels,
-            AVERAGE_WINDOW_DURATION_MS,
-            DETECTION_THRESHOLD,
-            SUPPRESSION_MS,
-            MINIMUM_COUNT,
-            MINIMUM_TIME_BETWEEN_SAMPLES_MS);
-
-    String actualModelFilename = MODEL_FILENAME.split("file:///android_asset/", -1)[1];
+  //Try to load model by khanh
+    String actualModelFilename = MODEL_FILEPATH.split("file:///android_asset/", -1)[1];
     try {
       tfLite = new Interpreter(loadModelFile(getAssets(), actualModelFilename));
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
 
-    tfLite.resizeInput(0, new int[] {RECORDING_LENGTH, 1});
-    tfLite.resizeInput(1, new int[] {1});
+    //Setup tensor for input day khanh
+    tfLite.resizeInput(0, new int[]{MODEL_FRAME_LENGTH, 1});
+    //tfLite.resizeInput(1, new int[]{1});
 
     // Start the recording and recognition threads.
     requestMicrophonePermission();
     startRecording();
     startRecognition();
 
-    sampleRateTextView = findViewById(R.id.sample_rate);
-    inferenceTimeTextView = findViewById(R.id.inference_info);
-    bottomSheetLayout = findViewById(R.id.bottom_sheet_layout);
-    gestureLayout = findViewById(R.id.gesture_layout);
-    sheetBehavior = BottomSheetBehavior.from(bottomSheetLayout);
-    bottomSheetArrowImageView = findViewById(R.id.bottom_sheet_arrow);
-
-    threadsTextView = findViewById(R.id.threads);
-    plusImageView = findViewById(R.id.plus);
-    minusImageView = findViewById(R.id.minus);
-    apiSwitchCompat = findViewById(R.id.api_info_switch);
-
-    yesTextView = findViewById(R.id.yes);
-    noTextView = findViewById(R.id.no);
-    upTextView = findViewById(R.id.up);
-    downTextView = findViewById(R.id.down);
-    leftTextView = findViewById(R.id.left);
-    rightTextView = findViewById(R.id.right);
-    onTextView = findViewById(R.id.on);
-    offTextView = findViewById(R.id.off);
-    stopTextView = findViewById(R.id.stop);
-    goTextView = findViewById(R.id.go);
-
-    apiSwitchCompat.setOnCheckedChangeListener(this);
-
-    ViewTreeObserver vto = gestureLayout.getViewTreeObserver();
-    vto.addOnGlobalLayoutListener(
-        new ViewTreeObserver.OnGlobalLayoutListener() {
-          @Override
-          public void onGlobalLayout() {
-            gestureLayout.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-            int height = gestureLayout.getMeasuredHeight();
-
-            sheetBehavior.setPeekHeight(height);
-          }
-        });
-    sheetBehavior.setHideable(false);
-
-    sheetBehavior.setBottomSheetCallback(
-        new BottomSheetBehavior.BottomSheetCallback() {
-          @Override
-          public void onStateChanged(@NonNull View bottomSheet, int newState) {
-            switch (newState) {
-              case BottomSheetBehavior.STATE_HIDDEN:
-                break;
-              case BottomSheetBehavior.STATE_EXPANDED:
-                {
-                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_down);
-                }
-                break;
-              case BottomSheetBehavior.STATE_COLLAPSED:
-                {
-                  bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
-                }
-                break;
-              case BottomSheetBehavior.STATE_DRAGGING:
-                break;
-              case BottomSheetBehavior.STATE_SETTLING:
-                bottomSheetArrowImageView.setImageResource(R.drawable.icn_chevron_up);
-                break;
-            }
-          }
-
-          @Override
-          public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
-        });
-
-    plusImageView.setOnClickListener(this);
-    minusImageView.setOnClickListener(this);
-
-    sampleRateTextView.setText(SAMPLE_RATE + " Hz");
+    //UI processing code by khanh
+    outTextView = findViewById(R.id.textView);
   }
 
   private void requestMicrophonePermission() {
@@ -313,16 +209,16 @@ public class SpeechActivity extends Activity
     // Estimate the buffer size we'll need for this device.
     int bufferSize =
         AudioRecord.getMinBufferSize(
-            SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+                INPUT_SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
     if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
-      bufferSize = SAMPLE_RATE * 2;
+      bufferSize = INPUT_SAMPLE_RATE * 2;
     }
     short[] audioBuffer = new short[bufferSize / 2];
 
     AudioRecord record =
         new AudioRecord(
             MediaRecorder.AudioSource.DEFAULT,
-            SAMPLE_RATE,
+            INPUT_SAMPLE_RATE,
             AudioFormat.CHANNEL_IN_MONO,
             AudioFormat.ENCODING_PCM_16BIT,
             bufferSize);
@@ -384,14 +280,14 @@ public class SpeechActivity extends Activity
     recognitionThread = null;
   }
 
+
+
   private void recognize() {
 
     Log.v(LOG_TAG, "Start recognition");
 
-    short[] inputBuffer = new short[RECORDING_LENGTH];
-    float[][] floatInputBuffer = new float[RECORDING_LENGTH][1];
-    float[][] outputScores = new float[1][labels.size()];
-    int[] sampleRateList = new int[] {SAMPLE_RATE};
+    short[] inputBuffer = new short[MODEL_FRAME_LENGTH];
+    float[] floatInputBuffer = new float[MODEL_FRAME_LENGTH];
 
     // Loop, grabbing recorded data and running the recognition model on it.
     while (shouldContinueRecognition) {
@@ -401,147 +297,109 @@ public class SpeechActivity extends Activity
       // local version.
       recordingBufferLock.lock();
       try {
+        //Phong khi chua recording duoc nhieu (recordingbuffer co it du lieu luc moi bat dau ghi am)
         int maxLength = recordingBuffer.length;
         int firstCopyLength = maxLength - recordingOffset;
+
         int secondCopyLength = recordingOffset;
+
         System.arraycopy(recordingBuffer, recordingOffset, inputBuffer, 0, firstCopyLength);
         System.arraycopy(recordingBuffer, 0, inputBuffer, firstCopyLength, secondCopyLength);
       } finally {
         recordingBufferLock.unlock();
       }
 
-      // We need to feed in float values between -1.0f and 1.0f, so divide the
-      // signed 16-bit inputs.
-      for (int i = 0; i < RECORDING_LENGTH; ++i) {
-        floatInputBuffer[i][0] = inputBuffer[i] / 32767.0f;
+      //TODO down sample into model's sample rate, which is 16000
+      for (int i = 0; i < MODEL_FRAME_LENGTH; ++i) {
+        floatInputBuffer[i] = inputBuffer[i] * AMPLIFY_PARAM;
       }
+      float downsample_factor = INPUT_SAMPLE_RATE / MODEL_SAMPLE_RATE;
 
-      Object[] inputArray = {floatInputBuffer, sampleRateList};
-      Map<Integer, Object> outputMap = new HashMap<>();
-      outputMap.put(0, outputScores);
+      //TODO Process mic amplify
+
+      //TODO Extract overlapping
+
+
+
+      Object[] inputArray = {floatInputBuffer};
+      Map<Integer, Object> output_map = new TreeMap<>();
+      float[][][] frame_logits = new float[1][32][88];
+      float[][][] onset_logits = new float[1][32][88];
+      float[][][] offset_logits = new float[1][32][88];
+      float[][][] velocity_values = new float[1][32][88];
+
+      output_map.put(0, frame_logits);
+      output_map.put(1, onset_logits);
+      output_map.put(2, offset_logits);
+      output_map.put(3, velocity_values);
 
       // Run the model.
-      tfLite.runForMultipleInputsOutputs(inputArray, outputMap);
+      tfLite.runForMultipleInputsOutputs(inputArray, output_map);
 
       // Use the smoother to figure out if we've had a real recognition event.
       long currentTime = System.currentTimeMillis();
-      final RecognizeCommands.RecognitionResult result =
-          recognizeCommands.processLatestResults(outputScores[0], currentTime);
+      //final String result = outputScores[0];
       lastProcessingTimeMs = new Date().getTime() - startTime;
       runOnUiThread(
           new Runnable() {
             @Override
             public void run() {
+              String outputstring = "";
+              StringBuilder sb = new StringBuilder();
+              for(int i=0; i<32; i++)
+                for(int m=0; m<88; m++)
+                {
+                  if (onset_logits[0][i][m] > 2)
+                  sb.append(String.format("%d-%d: %f \n", (int)(m/12),m%12, onset_logits[0][i][m]));
 
-              inferenceTimeTextView.setText(lastProcessingTimeMs + " ms");
-
-              // If we do have a new command, highlight the right list entry.
-              if (!result.foundCommand.startsWith("_") && result.isNewCommand) {
-                int labelIndex = -1;
-                for (int i = 0; i < labels.size(); ++i) {
-                  if (labels.get(i).equals(result.foundCommand)) {
-                    labelIndex = i;
-                  }
                 }
-
-                switch (labelIndex - 2) {
-                  case 0:
-                    selectedTextView = yesTextView;
-                    break;
-                  case 1:
-                    selectedTextView = noTextView;
-                    break;
-                  case 2:
-                    selectedTextView = upTextView;
-                    break;
-                  case 3:
-                    selectedTextView = downTextView;
-                    break;
-                  case 4:
-                    selectedTextView = leftTextView;
-                    break;
-                  case 5:
-                    selectedTextView = rightTextView;
-                    break;
-                  case 6:
-                    selectedTextView = onTextView;
-                    break;
-                  case 7:
-                    selectedTextView = offTextView;
-                    break;
-                  case 8:
-                    selectedTextView = stopTextView;
-                    break;
-                  case 9:
-                    selectedTextView = goTextView;
-                    break;
-                }
-
-                if (selectedTextView != null) {
-                  selectedTextView.setBackgroundResource(R.drawable.round_corner_text_bg_selected);
-                  final String score = Math.round(result.score * 100) + "%";
-                  selectedTextView.setText(selectedTextView.getText() + "\n" + score);
-                  selectedTextView.setTextColor(
-                      getResources().getColor(android.R.color.holo_orange_light));
-                  handler.postDelayed(
-                      new Runnable() {
-                        @Override
-                        public void run() {
-                          String origionalString =
-                              selectedTextView.getText().toString().replace(score, "").trim();
-                          selectedTextView.setText(origionalString);
-                          selectedTextView.setBackgroundResource(
-                              R.drawable.round_corner_text_bg_unselected);
-                          selectedTextView.setTextColor(
-                              getResources().getColor(android.R.color.darker_gray));
-                        }
-                      },
-                      750);
-                }
-              }
+              outputstring = sb.toString();
+              outTextView.setText(String.format("%d ms: %s", lastProcessingTimeMs, outputstring));
             }
           });
+
       try {
-        // We don't need to run too frequently, so snooze for a bit.
+        // We don't need to run too frequently, so snooze for a bit and save some battery.
         Thread.sleep(MINIMUM_TIME_BETWEEN_SAMPLES_MS);
       } catch (InterruptedException e) {
         // Ignore
       }
+
     }
 
     Log.v(LOG_TAG, "End recognition");
   }
 
-  @Override
-  public void onClick(View v) {
-    if (v.getId() == R.id.plus) {
-      String threads = threadsTextView.getText().toString().trim();
-      int numThreads = Integer.parseInt(threads);
-      numThreads++;
-      threadsTextView.setText(String.valueOf(numThreads));
-      //            tfLite.setNumThreads(numThreads);
-      int finalNumThreads = numThreads;
-      backgroundHandler.post(() -> tfLite.setNumThreads(finalNumThreads));
-    } else if (v.getId() == R.id.minus) {
-      String threads = threadsTextView.getText().toString().trim();
-      int numThreads = Integer.parseInt(threads);
-      if (numThreads == 1) {
-        return;
-      }
-      numThreads--;
-      threadsTextView.setText(String.valueOf(numThreads));
-      tfLite.setNumThreads(numThreads);
-      int finalNumThreads = numThreads;
-      backgroundHandler.post(() -> tfLite.setNumThreads(finalNumThreads));
-    }
-  }
-
-  @Override
-  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-    backgroundHandler.post(() -> tfLite.setUseNNAPI(isChecked));
-    if (isChecked) apiSwitchCompat.setText("NNAPI");
-    else apiSwitchCompat.setText("TFLITE");
-  }
+//  @Override
+//  public void onClick(View v) {
+//    if (v.getId() == R.id.plus) {
+//      String threads = threadsTextView.getText().toString().trim();
+//      int numThreads = Integer.parseInt(threads);
+//      numThreads++;
+//      threadsTextView.setText(String.valueOf(numThreads));
+//      //            tfLite.setNumThreads(numThreads);
+//      int finalNumThreads = numThreads;
+//      backgroundHandler.post(() -> tfLite.setNumThreads(finalNumThreads));
+//    } else if (v.getId() == R.id.minus) {
+//      String threads = threadsTextView.getText().toString().trim();
+//      int numThreads = Integer.parseInt(threads);
+//      if (numThreads == 1) {
+//        return;
+//      }
+//      numThreads--;
+//      threadsTextView.setText(String.valueOf(numThreads));
+//      tfLite.setNumThreads(numThreads);
+//      int finalNumThreads = numThreads;
+//      backgroundHandler.post(() -> tfLite.setNumThreads(finalNumThreads));
+//    }
+//  }
+//
+//  @Override
+//  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//    backgroundHandler.post(() -> tfLite.setUseNNAPI(isChecked));
+//    if (isChecked) apiSwitchCompat.setText("NNAPI");
+//    else apiSwitchCompat.setText("TFLITE");
+//  }
 
   private static final String HANDLE_THREAD_NAME = "CameraBackground";
 
